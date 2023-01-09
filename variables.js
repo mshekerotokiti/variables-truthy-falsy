85e0398d164b922f2822f62165c032bd5eca4733
mshekerotokiti <sharontokiti@gmail.com>
2023-01-09 06:55:23 +0300

    second commit

--- variables.js
+++ variables.js
@@ -1,10 +1,11 @@
-console.log ()
+//5 falsey values null, '' , NaN , 0 , Undefined
 const age =14;
-let isAdult;
-if (age >=18)
+let isAdult=age >=18
+if (isAdult)
 {
-    isAdult = true;
+    `Jane will have a 20 year old boyfriend`
 }
 else {
-    isAdult = false;
+`Jane will not be allowed to have a 20 year old boyfriend`
 }
+console.log(isAdult)
